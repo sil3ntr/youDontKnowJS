2dee670d010f78b4a7397d341e9da2302ce02764
sil3ntr <richard@sil3ntsecurity.com>
2018-04-08 19:08:11 +0800

    added code

---
+++ ch1_ex1.js
@@ -0,0 +1,25 @@
+const TAX_RATE = 0.1;
+const PHONE_PRICE = 199;
+const ACCESSORY_PRICE = 19.99;
+const SPENDING_THRESHOLD = 500;
+var bankBal = 1000;
+
+function purchase (bankBal){
+    //do some purchasing
+    while (bank >= 0 ){
+        bank = bank - PHONE_PRICE;
+        
+    }
+    return bank;
+
+}
+
+
+function priceFormat () {
+
+}
+
+purchase;
+
+console.log(bank);
+console.log(bankBal);
